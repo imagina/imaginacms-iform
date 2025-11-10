af8a64b679c93333a08bd6b17eae67348ccdbc0c
William <williamverde10@gmail.com>
2025-11-10 10:44:19 -0400

    Added: translations settings

---
+++ resources/lang/es/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'incrementalCode' => 'Codigo Incremental (Este valor se incrementa automaticamente con cada nuevo registro del Form PQRSF)',
+    'formCode' => 'Codigo del Formulario (Codigo que identifica el formulario PQRSF)',
+
+];
